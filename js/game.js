55fd36e46fabd6bbd5bbb26abaadab8a2e7ef533
Simon Schaffer <SimonSchaffer1@gmail.com>
2014-07-11 23:01:39 -0700

    Added game states

--- js/game.js
+++ js/game.js
@@ -1,14 +1,15 @@
 var RT = RT || {};
 
-RT.Game = function(renderParent, size) {
-	this.bounds = [
-		new RT.Line(new RT.Vc(0, 0), new RT.Vc(1, 0)), // Left Edge
-		new RT.Line(new RT.Vc(0, 0), new RT.Vc(0, 1)), // Bottom Edge
-		new RT.Line(new RT.Vc(size.x, 0), new RT.Vc(-1, 0)), // Right Edge
-		new RT.Line(new RT.Vc(0,size.y), new RT.Vc(0, -1)) // Top Edge
-	];
-	this.rangeBounds = [];
+RT.GS = {
+	Roam: "roam",
+	Move: "move",
+	ChooseFirst: "choose_first",
+	ChooseFullAttack: "choose_full_attack",
+	ChooseAttack: "choose_attack",
+};
 
+
+RT.Game = function(renderParent, size) {
 	// Render stuff
 	container = this.container = new PIXI.DisplayObjectContainer();
 	renderParent.addChild(container);
@@ -30,10 +31,19 @@ RT.Game = function(renderParent, size) {
 	enemy.position.y = 400;
 	container.addChild(enemy);
 
-	this.combatMode = false;
-	this.playerRange = 2;
-	var me = this;
-	kd.C.press(function YourFae() { me.ToggleCombat(); });
+	// Game State
+	this.TransitionState(RT.GS.Roam);
+	this.bounds = [
+		new RT.Line(new RT.Vc(0, 0), new RT.Vc(1, 0)), // Left Edge
+		new RT.Line(new RT.Vc(0, 0), new RT.Vc(0, 1)), // Bottom Edge
+		new RT.Line(new RT.Vc(size.x, 0), new RT.Vc(-1, 0)), // Right Edge
+		new RT.Line(new RT.Vc(0,size.y), new RT.Vc(0, -1)) // Top Edge
+	];
+	this.rangeBounds = [];
+	this.playerRange = 3;
+
+	// Action Hooks
+	this.BindKeys();
 };
 
 RT.MakeUnit = function(color) {
@@ -45,76 +55,135 @@ RT.MakeUnit = function(color) {
 	return unit;
 }
 
-RT.Game.prototype.ToggleCombat = function() {
-	if (this.combatMode) {
-		this.combatMode = false;
-		this.gridRender.visible = false;
+RT.Game.prototype.TransitionState = function(newState) {
+	/*
+	 *  Actions Upon Exiting Previous State
+	 */
+
+	// Roaming
+	if (this.currentState == RT.GS.Roam) {
+		this.gridRender.visible = true;
+	}
+	// Move
+	else if (this.currentState == RT.GS.Move) {
 		this.range.clear();
 		this.rangeBounds = [];
-	} else {
-		this.combatMode = true;
-		this.gridRender.visible = true;
-
-		// Render movement range
-		var hx = this.playerCenter = this.grid.GetGridCoord(this.player.position);
-
-		var rg = this.playerRange;
-		for (var x = -rg; x <= rg; ++x) {
-			for (var y = -rg; y <= rg; ++y) {
-				if (Math.abs(x + y) <= rg) {
-					this.range.beginFill(0x0000FF, 1);
-					this.range.lineStyle(4, 0x000066, 1);
-					this.grid.RenderHex(new RT.Vc(hx.x+x, hx.y+y), this.range);
-				}
-			}
-		}
-
-		// Update movement bounds
-		var axes = [
-			this.grid.GetBasis(0).Add( this.grid.GetBasis(1) ),
-			this.grid.GetBasis(0).Add( this.grid.GetBasis(2) ),
-			this.grid.GetBasis(1).Add( this.grid.GetBasis(2).Negate() )
-		];
-
-		for (var i = 0; i < 3; ++i) {
-			var axis = axes[i];
-			axis = axis.Mult(.5*this.playerRange);
-			dir = axis.Normalize();
-			var center = this.grid.GetRealCoord(hx);
-
-			this.rangeBounds.push(
-				new RT.Line(center.Add(axis), dir.Negate())
-			);
-			this.rangeBounds.push(
-				new RT.Line(center.Add(axis.Negate()), dir)
-			);
-		}
 	}
-};
+
+	/*
+	 *  Actions Upon Entering New State
+	 */
+
+	// Roaming
+	if (newState == RT.GS.Roam) {
+		this.SetMessage("Just Saccin' Around");
+		this.SetAction("&nbsp;");
+		this.ButtonHandler = null;
+
+		// Remove combat overlay grid
+		this.gridRender.visible = false;
+	}
+	// Choose First
+	else if (newState == RT.GS.ChooseFirst) {
+		this.SetMessage("You have entered combat!");
+		this.SetAction("Press 'M' to move or 'F' for a full attack");
+		this.ButtonHandler = "ChooseFirstHandler";
+	}
+	// Move
+	else if (newState == RT.GS.Move) {
+		this.SetAction("Press 'Spacebar' When Done");
+		this.ButtonHandler = "MoveHandler";
+
+		this.ShowBounds();
+	}
+	// Choose Attack
+	else if (newState == RT.GS.ChooseFullAttack || newState == RT.GS.ChooseAttack) {
+		this.SetAction("Choose Your Attack!");
+		this.ButtonHandler = null;
+	}
+
+	this.currentState = newState;
+}
+
+RT.Game.prototype.ChooseFirstHandler = function(btn) {
+	if ('m' == btn) {
+		this.TransitionState(RT.GS.Move);
+	} else if ('f' == btn) {
+		this.TransitionState(RT.GS.ChooseFullAttack);
+	}
+}
+
+RT.Game.prototype.MoveHandler = function(btn) {
+	if (btn == 'space') {
+		this.TransitionState(RT.GS.ChooseAttack);
+	}
+}
 
 RT.Game.prototype.Update = function() {
-	var speed = 5;
-	var dir = new RT.Vc();
+	if (this.currentState == RT.GS.Roam || this.currentState == RT.GS.Move) {
+		var speed = 5;
+		var dir = new RT.Vc();
 
-	if (kd.W.isDown()) dir.y += 1;
-	if (kd.A.isDown()) dir.x -= 1;
-	if (kd.S.isDown()) dir.y -= 1;
-	if (kd.D.isDown()) dir.x += 1;
+		if (kd.W.isDown()) dir.y += 1;
+		if (kd.A.isDown()) dir.x -= 1;
+		if (kd.S.isDown()) dir.y -= 1;
+		if (kd.D.isDown()) dir.x += 1;
 
-	dir = dir.Normalize();
+		dir = dir.Normalize();
 
-	this.player.position.x += dir.x * speed;
-	this.player.position.y += dir.y * speed;
+		this.player.position.x += dir.x * speed;
+		this.player.position.y += dir.y * speed;
 
-	this.DoCollisions(this.player.position, this.bounds.concat(this.rangeBounds));
+		this.DoCollisions(this.player.position, this.bounds.concat(this.rangeBounds));
 
-	if (!this.combatMode &&
-		new RT.Vc(this.player.position).Add(new RT.Vc(this.enemy.position).Negate()).Len() < 180) {
-		this.ToggleCombat();
+		if (this.currentState == RT.GS.Roam) {
+			var p = new RT.Vc(this.player.position);
+			var e = new RT.Vc(this.enemy.position);
+			if (p.Add(e.Negate()).Len() < 180)
+				this.TransitionState(RT.GS.ChooseFirst);
+		}
 	}
-	console.log(JSON.stringify(new RT.Vc(this.player.position)));
-	//console.log(JSON.stringify(this.player.position));
+	//console.log(JSON.stringify(this.player.position)); becomes null at some point?
 };
+
+RT.Game.prototype.ShowBounds = function() {
+	// Render movement range
+	var hx = this.playerCenter = this.grid.GetGridCoord(this.player.position);
+
+	var rg = this.playerRange;
+	for (var x = -rg; x <= rg; ++x) {
+		for (var y = -rg; y <= rg; ++y) {
+			if (Math.abs(x + y) <= rg) {
+				this.range.beginFill(0x0000FF, 1);
+				this.range.lineStyle(4, 0x000066, 1);
+				this.grid.RenderHex(new RT.Vc(hx.x+x, hx.y+y), this.range);
+			}
+		}
+	}
+
+	// Update movement bounds
+	var axes = [
+		this.grid.GetBasis(0).Add( this.grid.GetBasis(1) ),
+		this.grid.GetBasis(0).Add( this.grid.GetBasis(2) ),
+		this.grid.GetBasis(1).Add( this.grid.GetBasis(2).Negate() )
+	];
+
+	for (var i = 0; i < 3; ++i) {
+		var axis = axes[i];
+		axis = axis.Mult(.5*this.playerRange);
+		dir = axis.Normalize();
+		var center = this.grid.GetRealCoord(hx);
+
+		this.rangeBounds.push(
+			new RT.Line(center.Add(axis), dir.Negate())
+		);
+		this.rangeBounds.push(
+			new RT.Line(center.Add(axis.Negate()), dir)
+		);
+	}
+
+	console.log("bunds: " + JSON.stringify(this.rangeBounds));
+}
 
 RT.Game.prototype.DoCollisions = function(pt, bounds) {
 	var len = bounds.length;
@@ -125,3 +194,26 @@ RT.Game.prototype.DoCollisions = function(pt, bounds) {
 		pt.y += fix.y;
 	}
 };
+
+RT.Game.prototype.SetMessage = function(msg) {
+	document.getElementById('message').innerHTML = msg;
+}
+
+RT.Game.prototype.SetAction = function(msg) {
+	document.getElementById('action').innerHTML = msg;
+}
+
+RT.Game.prototype.ButtonPress = function(btn) {
+	if (this.ButtonHandler != null) {
+		this[this.ButtonHandler](btn);
+	}
+}
+
+RT.Game.prototype.BindKeys = function() {
+	var me = this;
+	kd.C.press(function() { me.ButtonPress('c'); });
+	kd.M.press(function() { me.ButtonPress('m'); });
+	kd.F.press(function() { me.ButtonPress('f'); });
+	kd.ENTER.press(function() { me.ButtonPress('enter'); });
+	kd.SPACE.press(function() { me.ButtonPress('space'); });
+}
